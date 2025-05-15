e9da50a204ef7a30e610220551ab097bbaeba079
Shinra88 <chokdingo@hotmail.com>
2025-05-15 19:39:28 +0200

    correction editbook and deletecomment

--- src/Modals/CommentModerationModal/CommentModerationModal.jsx
+++ src/Modals/CommentModerationModal/CommentModerationModal.jsx
@@ -1,31 +1,34 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './CommentModerationModal.module.css';
 import avatarDefault from '../../images/avatar.png';
 import { displayStars } from '../../utils/helpers';
 import { deleteCommentAsAdmin } from '../../services/commentService';
 import FeatherIcon from '../../images/feather.png';
-
+import ToastSuccess from '../../components/ToastSuccess/ToastSuccess';
 
 function CommentModerationModal({ bookId, comments, onClose, onUpdate }) {
-    const modalRef = useRef();
-  
-    useEffect(() => {
-      const handleClickOutside = (event) => {
-        if (modalRef.current && !modalRef.current.contains(event.target)) {
-          onClose();
-        }
-      };
-  
-      document.addEventListener('mousedown', handleClickOutside);
-      return () => document.removeEventListener('mousedown', handleClickOutside);
-    }, [onClose]);
+  const modalRef = useRef();
+  const [showToast, setShowToast] = useState(false);
 
-    const handleDelete = async (commentId) => {
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [onClose]);
+
+  const handleDelete = async (commentId) => {
     try {
       await deleteCommentAsAdmin(commentId);
       const updated = comments.filter((c) => c.commentId !== commentId);
       onUpdate(updated);
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 2000);
     } catch (error) {
       console.error('Erreur suppression commentaire admin :', error);
     }
@@ -35,6 +38,9 @@ function CommentModerationModal({ bookId, comments, onClose, onUpdate }) {
     <div className={styles.modalBackground}>
       <div ref={modalRef} className={styles.modalContent}>
         <h2>Modération des commentaires</h2>
+
+        {showToast && <ToastSuccess message="Commentaire supprimé 🎉" />}
+
         {comments.length === 0 && <p>Aucun commentaire pour ce livre.</p>}
         {comments.map((comment) => (
           <div key={comment.commentId} className={styles.commentBlock}>
@@ -69,7 +75,6 @@ function CommentModerationModal({ bookId, comments, onClose, onUpdate }) {
     </div>
   );
 }
-
 
 CommentModerationModal.propTypes = {
   bookId: PropTypes.number.isRequired,
