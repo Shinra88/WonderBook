009e32a1fd9f0e90ad9e52dc011e834efa92d917
Shinra88 <chokdingo@hotmail.com>
2025-07-26 09:53:46 +0200

    patch home css v2

--- src/components/Books/BookDisplay/BookDisplay.jsx
+++ src/components/Books/BookDisplay/BookDisplay.jsx
@@ -128,7 +128,7 @@ function BookDisplay({ book, size, showDetails = false, hideImage = false, admin
       title = (
         <>
           <h2 className={styles.MainTitle}>{mainTitle || 'Missing title'}</h2>
-          {subTitle && <h4 className={styles.Subtitle}>{subTitle || 'Missing subtitle'}</h4>}
+          {subTitle && <h3 className={styles.Subtitle}>{subTitle || 'Missing subtitle'}</h3>}
         </>
       );
       break;
@@ -136,7 +136,7 @@ function BookDisplay({ book, size, showDetails = false, hideImage = false, admin
       title = (
         <>
           <h3 className={styles.MainTitle}>{mainTitle || 'Missing title'}</h3>
-          {subTitle && <h4 className={styles.Subtitle}>{subTitle || 'Missing subtitle'}</h4>}
+          {subTitle && <h3 className={styles.Subtitle}>{subTitle || 'Missing subtitle'}</h3>}
         </>
       );
       break;
@@ -144,7 +144,7 @@ function BookDisplay({ book, size, showDetails = false, hideImage = false, admin
       title = (
         <>
           <h2 className={styles.MainTitle}>{mainTitle || 'Missing title'}</h2>
-          {subTitle && <h4 className={styles.Subtitle}>{subTitle || 'Missing subtitle'}</h4>}
+          {subTitle && <h3 className={styles.Subtitle}>{subTitle || 'Missing subtitle'}</h3>}
         </>
       );
   }
